a57082d9cdf49d63f76909a4cb5c3fc308f826e0
Bartholomew Chwalek <bart.chwalek@gmail.com>
2022-03-04 17:11:23 -0500

    updates

--- src/__tests__/DeploySync.test.ts
+++ src/__tests__/DeploySync.test.ts
@@ -1,6 +1,6 @@
-import {DeploySync} from '../index';
+import { DeploySync } from '../index';
 
 test('DeploySync', () => {
-    const d = new DeploySync();
-    expect<string>(d.className).toBe('DeploySync');
-})
+  const d = new DeploySync();
+  expect<string>(d.className).toBe('DeploySync');
+});
--- src/rsync/index.ts
+++ src/rsync/index.ts
@@ -1,71 +1,69 @@
-import {CommandShell} from '../command-shell';
+import { CommandShell } from '../command-shell';
 
-export interface RsyncConfig {
+export interface RsyncConfig {}
 
-}
-
-export interface RsyncResults {
-
-}
+export interface RsyncResults {}
 
 export class Rsync extends CommandShell {
+  private source: string;
+  private destination: string;
 
-    private source: string;
-    private destination: string;
+  constructor(configuration: RsyncConfig) {
+    super('rsync', '-', {
+      flags: {
+        u: 'skip files that are newer on the receiver',
+        v: 'increase verbosity',
+      },
+    });
+  }
 
-    constructor(configuration: RsyncConfig) {
-        super('rsync', '-', {
-            flags: {
-                u: 'skip files that are newer on the receiver',
-                v: 'increase verbosity'
-            }
-        });
-    }
+  public asRemoteUser(username: string): Rsync {
+    return this;
+  }
 
-    public asRemoteUser(username: string): Rsync {
-        return this;
-    }
+  public include(): Rsync {
+    return this;
+  }
 
-    public include(): Rsync {
-        return this;
-    }
+  public exclude(): Rsync {
+    return this;
+  }
 
-    public exclude(): Rsync {
-        return this;
-    }
+  public update(): Rsync {
+    this.addFlag('u');
+    return this;
+  }
 
-    public update(): Rsync {
-        this.addFlag('u');
-        return this;
-    }
+  public verbose(): Rsync {
+    this.addFlag('v');
+    return this;
+  }
 
-    public verbose(): Rsync {
-        this.addFlag('v');
-        return this;
-    }
+  public overwrite(): Rsync {
+    return this;
+  }
 
-    public overwrite(): Rsync {
-        return this;
-    }
+  public async sync(): Promise<RsyncResults> {
+    return new Promise<RsyncResults>((resolve, reject) => {
+      this.spawn()
+        .onClose((reason) => {
+          console.log(reason);
+          resolve({} as RsyncResults);
+        })
+        .onError((error) => {
+          console.log('Error in command: ', this.command);
+          console.log(error);
+          reject(error);
+        })
+        .execute();
+    });
+  }
 
-    public async sync(): Promise<RsyncResults> {
-        return new Promise<RsyncResults>(((resolve, reject) => {
-            this.spawn().onClose((reason) => {
-                console.log(reason);
-                resolve({} as RsyncResults);
-            }).onError((error) => {
-                console.log('Error in command: ', this.command);
-                console.log(error);
-                reject(error);
-            }).execute();
-        }));
-    }
+  public from(): Rsync {
+    return this;
+  }
 
-    public from(): Rsync {
-        return this;
-    }
-
-    public to(): Rsync {
-        return this;
-    }
+  public to(): Rsync {
+    return this;
+  }
 }
