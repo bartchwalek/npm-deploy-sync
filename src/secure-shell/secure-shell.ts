add850bf5aa3e9f7469f048e5cba0f4cdf36c529
Bartholomew Chwalek <bart.chwalek@gmail.com>
2022-03-14 14:15:58 -0400

    added verbose to secure shell

--- src/secure-shell/secure-shell.ts
+++ src/secure-shell/secure-shell.ts
@@ -1,89 +1,94 @@
-import { CommandShell, ExecutionController } from '../command-shell';
+import {CommandShell, ExecutionController} from '../command-shell';
 
 export class SecureShell extends CommandShell {
-  private remote: string = '';
+    private remote: string = '';
 
-  private remoteHost: string;
-  private remotePort: number = 22;
-  private remoteUser: string;
-  private remotePass: string;
+    private remoteHost: string;
+    private remotePort: number = 22;
+    private remoteUser: string;
+    private remotePass: string;
 
-  private p;
+    private p;
 
-  private dataToWriteOnConnect: string;
+    private dataToWriteOnConnect: string;
 
-  constructor(debugLevel: number = 5) {
-    super(
-      'ssh',
-      '-',
-      {
-        flags: {},
-      },
-      (arg) => arg,
-      '$flags $options $map[dest]',
-      () => {
-        this.setupRemote();
-      },
-      () => {
-        this.connect();
-      },
-      debugLevel,
-    );
-  }
-
-  private setupRemote(): void {
-    this.remote = `${this.remoteHost} -p ${this.remotePort}`;
-    this.setMappedValue('dest', `${this.remoteUser ? this.remoteUser + '@' : ''}${this.remote}`);
-  }
-
-  public as(remoteUsr: string): SecureShell {
-    this.remoteUser = remoteUsr;
-    return this;
-  }
-
-  public disableHostChecking(): SecureShell {
-    this.addOption('-oStrictHostKeyChecking=no');
-    return this;
-  }
-
-  public password(remotePass: string): SecureShell {
-    if (!this.remotePass) {
-      this.argumentsPreprocessMap = '$special ' + this.argumentsPreprocessMap;
-      this.command = 'sshpass';
+    constructor(debugLevel: number = 5) {
+        super(
+            'ssh',
+            '-',
+            {
+                flags: {},
+            },
+            (arg) => arg,
+            '$flags $options $map[dest]',
+            () => {
+                this.setupRemote();
+            },
+            () => {
+                return this.connect();
+            },
+            debugLevel,
+        );
     }
 
-    this.remotePass = remotePass;
-    this.addSpecial(['-p', `${this.remotePass}`, 'ssh']);
-    return this;
-  }
-
-  public to(remoteHost: string): SecureShell {
-    this.remoteHost = remoteHost;
-    return this;
-  }
-
-  public port(remotePort: number): SecureShell {
-    this.remotePort = remotePort;
-    return this;
-  }
-
-  public async connect() {
-    let dataToWrite: string;
-    if (this.dataToWriteOnConnect) {
-      dataToWrite = this.dataToWriteOnConnect + '\n';
+    private setupRemote(): void {
+        this.remote = `${this.remoteHost} -p ${this.remotePort}`;
+        this.setMappedValue('dest', `${this.remoteUser ? this.remoteUser + '@' : ''}${this.remote}`);
     }
 
-    return new Promise<ExecutionController>((resolve, reject) => {
-      this.p = this.spawn();
-      this.p
-        .onConnect(() => {
-          resolve(this.p);
-        })
-        .write(dataToWrite)
-        .onData((data) => {
-          this.isDebug() && console.log(data);
-        })
-        .execute();
-    });
-  }
+    public verbose(): SecureShell {
+        this.addFlag('v');
+        return this;
+    }
+
+    public as(remoteUsr: string): SecureShell {
+        this.remoteUser = remoteUsr;
+        return this;
+    }
+
+    public disableHostChecking(): SecureShell {
+        this.addOption('-oStrictHostKeyChecking=no');
+        return this;
+    }
+
+    public password(remotePass: string): SecureShell {
+        if (!this.remotePass) {
+            this.argumentsPreprocessMap = '$special ' + this.argumentsPreprocessMap;
+            this.command = 'sshpass';
+        }
+
+        this.remotePass = remotePass;
+        this.addSpecial(['-p', `${this.remotePass}`, 'ssh']);
+        return this;
+    }
+
+    public to(remoteHost: string): SecureShell {
+        this.remoteHost = remoteHost;
+        return this;
+    }
+
+    public port(remotePort: number): SecureShell {
+        this.remotePort = remotePort;
+        return this;
+    }
+
+    public async connect() {
+        let dataToWrite: string;
+        if (this.dataToWriteOnConnect) {
+            dataToWrite = this.dataToWriteOnConnect + '\n';
+        }
+
+        return new Promise<ExecutionController>((resolve, reject) => {
+            this.p = this.spawn();
+            this.p
+                .onConnect(() => {
+                    resolve(this.p);
+                })
+                .write(dataToWrite)
+                .onData((data) => {
+                    this.isDebug() && console.log(data);
+                })
+                .execute();
+        });
+    }
 }
